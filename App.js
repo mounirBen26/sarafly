bdac34f6610cabe34ae6752884c3bdd99ba7bed5
mounirBen26 <mounir.bmh@hotmail.fr>
2024-06-20 19:03:05 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './screens/home';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Home />
+    </NavigationContainer>
   );
 }
 
